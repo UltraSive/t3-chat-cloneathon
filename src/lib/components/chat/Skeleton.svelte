280194732edd35c93561a4cf7aaad1c7f15c373e
ultrasive <github@aidanperry.net>
2025-06-12 17:18:47 -0500

    make chat skeleon its own load component

---
+++ src/lib/components/chat/Skeleton.svelte
@@ -0,0 +1,32 @@
+<script>
+	import { Skeleton } from '$lib/components/ui/skeleton';
+
+	import { User, Bot } from 'lucide-svelte';
+</script>
+
+<div class="bg-muted/20 mb-6 flex flex-row-reverse gap-4 rounded-lg p-4">
+	<div class="bg-primary flex h-8 w-8 shrink-0 items-center justify-center rounded-md select-none">
+		<User class="h-4 w-4 text-white" />
+	</div>
+
+	<div class="flex-1 space-y-2 overflow-hidden px-1 text-right">
+		<div class="font-semibold"><Skeleton class="h-4 w-8" /></div>
+		<Skeleton class="h-4 w-full" />
+		<Skeleton class="h-4 w-3/4" />
+	</div>
+</div>
+<div class="mb-6 flex gap-4 rounded-lg p-4">
+	<div class="bg-muted flex h-8 w-8 shrink-0 items-center justify-center rounded-md select-none">
+		<Bot class="text-foreground h-4 w-4" />
+	</div>
+
+	<div class="flex-1 space-y-2 overflow-hidden px-1">
+		<div class="font-semibold"><Skeleton class="h-4 w-24" /></div>
+		<Skeleton class="h-4 w-full" />
+		<Skeleton class="h-4 w-full" />
+		<Skeleton class="h-4 w-5/6" />
+		<Skeleton class="h-4 w-full" />
+		<Skeleton class="h-4 w-4/5" />
+		<Skeleton class="h-4 w-3/4" />
+	</div>
+</div>
--- src/routes/(chat)/chat/[thread]/+page.svelte
+++ src/routes/(chat)/chat/[thread]/+page.svelte
@@ -6,10 +6,10 @@
 
 	import Content from '$lib/components/chat/Content.svelte';
 	import MessageInput from '$lib/components/chat/MessageInput.svelte';
+	import ChatSkeleton from '$lib/components/chat/Skeleton.svelte';
 	import LoadError from '$lib/components/chat/LoadError.svelte';
 
 	import { ScrollArea } from '$lib/components/ui/scroll-area';
-	import { Skeleton } from '$lib/components/ui/skeleton';
 
 	import { User, Bot } from 'lucide-svelte';
 
@@ -36,36 +36,7 @@
 	<ScrollArea class="flex-1 p-4">
 		<div class="mx-auto max-w-3xl">
 			{#if query.isLoading}
-				<div class="mb-6 flex flex-row-reverse gap-4 rounded-lg p-4 bg-muted/20">
-					<div
-						class="bg-primary flex h-8 w-8 shrink-0 items-center justify-center rounded-md select-none"
-					>
-						<User class="h-4 w-4 text-white" />
-					</div>
-
-					<div class="flex-1 space-y-2 overflow-hidden px-1 text-right">
-						<div class="font-semibold"><Skeleton class="h-4 w-8" /></div>
-						<Skeleton class="h-4 w-full" />
-						<Skeleton class="h-4 w-3/4" />
-					</div>
-				</div>
-				<div class="mb-6 flex gap-4 rounded-lg p-4">
-					<div
-						class="bg-muted flex h-8 w-8 shrink-0 items-center justify-center rounded-md select-none"
-					>
-						<Bot class="text-foreground h-4 w-4" />
-					</div>
-
-					<div class="flex-1 space-y-2 overflow-hidden px-1">
-						<div class="font-semibold"><Skeleton class="h-4 w-24" /></div>
-						<Skeleton class="h-4 w-full" />
-				<Skeleton class="h-4 w-full" />
-				<Skeleton class="h-4 w-5/6" />
-				<Skeleton class="h-4 w-full" />
-				<Skeleton class="h-4 w-4/5" />
-				<Skeleton class="h-4 w-3/4" />
-					</div>
-				</div>
+				<ChatSkeleton />
 			{:else if query.error}
 				<LoadError error={query.error.toString()} />
 			{:else}
